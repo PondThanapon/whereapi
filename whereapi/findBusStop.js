b0bffb138977a90c835e351b208befb8751a86b4
thanapon19 <thanapon@iapp.co.th>
2020-02-15 16:35:03 +0700

    FisrtCommit

---
+++ whereapi/findBusStop.js
@@ -0,0 +1,23 @@
+
+// let positions = [
+//     { latitude: 18.306707, longitude: 99.398720, name: "ppp" },
+//     { latitude: 18.304112, longitude: 99.402893, name: "q" },
+//     { latitude: 18.302839, longitude: 99.404612, name: "r" },
+//     { latitude: 36.106, longitude: -97.082, name: "s" },
+// ];
+
+// let x = 18.302839;
+// let y = 99.404612;
+
+// let tmp = []
+
+// positions.filter(p => tmp.push({ name: p.name, data: Math.hypot(p.latitude - x, p.longitude - y) }))
+
+// function getYs() {
+//     return tmp.map(d => d.data);
+// }
+
+// let result = tmp.find( ({ data }) => data === Math.min(...getYs()) );
+
+// console.log(tmp)
+// console.log('route closest === ',result.name)
